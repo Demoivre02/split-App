90fcfb35c8ddee44e8586918ed89c4e23121debf
Demoivre02 <105006171+Demoivre02@users.noreply.github.com>
2022-07-06 14:00:27 +0100

    ulpoaded ffiles not folder

---
+++ tip.js
@@ -0,0 +1,111 @@
+const Bill= document.querySelector("#bill")
+const People = document.querySelector("#number")
+const five= document.querySelector(".five")
+const ten= document.querySelector(".ten")
+const fifteen= document.querySelector(".fifteen")
+const twenty5= document.querySelector(".twenty-five")
+const fifty= document.querySelector(".fifty")
+const custom= document.querySelector(".custom")
+const tip= document.querySelector(".tip")
+const total= document.querySelector(".total") 
+const Reset= document.querySelector(".lite")
+
+function handleInput(){
+    var p= People.value
+    var n = Bill.value  
+    var x;
+}
+function fivepercent(){
+    var p= People.value
+    x= Bill.value 
+    var n = Bill.value
+    if(People.value == ""){
+        alert("Please input the number of people before selecting Tip")
+    }else{
+       
+        tip.innerHTML= (((5/100)*n)/p)
+        let ToatlAmount= (+((5/100)*n) + +x)
+        total.innerHTML= ToatlAmount/p
+    }
+}
+
+
+function tenpercent(){
+    var p=  People.value
+    x= Bill.value
+    var n = Bill.value  
+    if(People.value == ""){
+        alert("Please input the number of people before selecting Tip")
+    }else{
+       
+        tip.innerHTML= (((10/100)*n)/p)
+        let ToatlAmount= (+((10/100)*n) + +x)
+        total.innerHTML= ToatlAmount/p
+    }
+}
+function fifteenpercent(){
+    var p=  People.value
+    x= Bill.value
+    var n = Bill.value  
+    if(People.value == ""){
+        alert("Please input the number of people before selecting Tip")
+    }else{
+       
+        tip.innerHTML= (((15/100)*n)/p)
+        let ToatlAmount= (+((15/100)*n) + +x)
+        total.innerHTML= ToatlAmount/p
+    }
+   
+}
+function twentyfivepercent(){
+    var p=  People.value
+    x= Bill.value
+    var n = Bill.value  
+    if(People.value == ""){
+        alert("Please input the number of people before selecting Tip")
+    }else{
+       
+        tip.innerHTML= (((25/100)*n)/p)
+        let ToatlAmount= (+((25/100)*n) + +x)
+        total.innerHTML= ToatlAmount/p
+    }
+}
+function fiftypercent(){
+    var p=  People.value  
+    x= Bill.value
+    var n = Bill.value  
+    if(People.value == ""){
+        alert("Please input the number of people before selecting Tip")
+    }else{
+       
+        tip.innerHTML= (((50/100)*n)/p)
+        let ToatlAmount= (+((50/100)*n) + +x)
+        total.innerHTML= ToatlAmount/p``
+    }
+}
+
+function toogleTheme(){
+  let envelope=  document.querySelector(".firstchild")
+  let Conainer= document.querySelector(".envelope")
+  let basket= document.querySelector(".container")
+  envelope.classList.toggle("themeChanger")
+  Conainer.classList.toggle("themeChanger")
+  basket.classList.toggle("themeChanger")
+
+}
+
+function Reload(){
+    document.location.reload()
+}
+
+
+
+
+
+
+
+
+
+
+
+
